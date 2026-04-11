6bb8fa1aa3a37c660f5fd8da39c4bcc200ed2e5f
visionsofparadise <matt@mattcavender.com>
2026-04-11 21:50:01 +0100

    chore: tailwind v4 and shadcn setup

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src/renderer'),
